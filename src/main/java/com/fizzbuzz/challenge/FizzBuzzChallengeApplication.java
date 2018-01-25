9c5178b9b9a5136fe4a19ca65208f4462fa528f8
NatashaLBallard <nbitcareer@gmail.com>
2018-01-25 11:26:36 -0500

    Initial commit

---
+++ src/main/java/com/fizzbuzz/challenge/FizzBuzzChallengeApplication.java
@@ -0,0 +1,12 @@
+package com.fizzbuzz.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizzBuzzChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FizzBuzzChallengeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/fizzbuzz/challenge/FizzBuzzChallengeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fizzbuzz.challenge;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FizzBuzzChallengeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

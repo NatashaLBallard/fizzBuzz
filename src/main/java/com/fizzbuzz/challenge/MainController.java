12c13e35b97b8db622bea3836b97eda7d37ab0f0
NatashaLBallard <nbitcareer@gmail.com>
2018-01-25 18:10:10 -0500

    Working on the tallying and displaying multiples of 7

--- src/main/java/com/fizzbuzz/challenge/MainController.java
+++ src/main/java/com/fizzbuzz/challenge/MainController.java
@@ -17,6 +17,10 @@ public class MainController {
 
         int x = 0;
         String storedNum = "";
+        String multiples17 = "";
+        String multiples23 = "";
+        int counter = 0;
+        counter++;
 
         double endValue = 0;
         Scanner keyboard = new Scanner(System.in);
@@ -47,8 +51,36 @@ public class MainController {
 
         }
 
+
+
         System.out.println();
-        return storedNum;
+        return storedNum ;
+    }
+
+
+    public String multipleTally()
+    {
+
+        int x = 0;
+        int multiples7 = 0;
+        int counter = 0;
+        counter++;
+        double endValue = 0;
+
+        while (x <= endValue-1) {
+            //System.out.println("<p>" + x + "</p>" );
+            x++;
+
+            if (Integer.valueOf(x) % 7 ==0){
+                multiples7++;
+            }
+
+
+        }
+        
+        System.out.println();
+        return (String.valueOf(multiples7)) ;
+
 
     }
 }

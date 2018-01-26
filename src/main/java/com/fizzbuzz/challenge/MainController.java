17cd0d704590b44975c41d5123c0da81c8d77166
NatashaLBallard <nbitcareer@gmail.com>
2018-01-26 14:23:08 -0500

    counts how many multiples, but doesn't display what they are

--- src/main/java/com/fizzbuzz/challenge/MainController.java
+++ src/main/java/com/fizzbuzz/challenge/MainController.java
@@ -17,12 +17,12 @@ public class MainController {
 
         int x = 0;
         String storedNum = "";
-       // String multiples7 = "";
-       // String multiples17 = "";
-       // String multiples23 = "";
-        //int counter7 = 0;
-       // int counter17 = 0;
-        //int counter27 = 0;
+        String multiples7 = "";
+        String multiples17 = "";
+        String multiples23 = "";
+        int counter7 = 0;
+        int counter17 = 0;
+        int counter23 = 0;
         double endValue = 0;
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Enter desired end value for the loop: ");
@@ -51,32 +51,32 @@ public class MainController {
             }
 
 
-           // while (x <= endValue-1) {
+            while (x <= endValue-1) {
                 //System.out.println("<p>" + x + "</p>" );
-              //  x++;
+                x++;
 
-              //  if (Integer.valueOf(x) % 7 ==0){
-              //      counter7 ++;
-             //   }
-             //   if (Integer.valueOf(x) % 17 ==0){
-             //       counter17 ++;
-              //  }
-              //  if (Integer.valueOf(x) % 27 ==0){
-            //        counter27 ++;
+                if (Integer.valueOf(x) % 7 ==0){
+                    counter7 ++;
+                }
+                if (Integer.valueOf(x) % 17 ==0){
+                    counter17 ++;
+                }
+                if (Integer.valueOf(x) % 23 ==0){
+                    counter23 ++;
                 }
 
             }
+
+        }
+
+
+
+
+        System.out.println();
+        return "End Value for Multiples Search: " + endValue + "<br>" + "Multiples of 7: " + String.valueOf(counter7) + "<br>" + "Multiples of 17: " + String.valueOf(counter17) + "<br>" + "Multiples of 23: " + String.valueOf(counter23) ;
 
 
     }
 
 
-
-        System.out.println();
-        return storedNum;
-    //+ "/br" //+ String.valueOf(counter7) + "/br" + String.valueOf(counter17) + "/br" + String.valueOf(counter27) ;
-
-
-
-
-
+}

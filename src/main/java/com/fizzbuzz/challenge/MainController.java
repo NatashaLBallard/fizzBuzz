eccffb85a798c81d583d194dcf576d3714c40ddc
NatashaLBallard <nbitcareer@gmail.com>
2018-01-26 17:26:51 -0500

    Displays numbers and the count of the multiples

--- src/main/java/com/fizzbuzz/challenge/MainController.java
+++ src/main/java/com/fizzbuzz/challenge/MainController.java
@@ -11,8 +11,7 @@ public class MainController {
 
 
     @RequestMapping("/")
-    public String fizzbuzz()
-    {
+    public String fizzbuzz() {
 
 
         int x = 0;
@@ -28,55 +27,72 @@ public class MainController {
         System.out.println("Enter desired end value for the loop: ");
         endValue = keyboard.nextDouble();
 
-        while (x <= endValue-1) {
-            //System.out.println("<p>" + x + "</p>" );
+        while (x <= endValue - 1) {
+            System.out.println("<p>" + x + "</p>");
             x++;
 
             System.out.println(storedNum);
 
-            if ((Integer.valueOf(x) % 3 ==0) && (Integer.valueOf(x) % 5 ==0)){
+            if ((Integer.valueOf(x) % 3 == 0) && (Integer.valueOf(x) % 5 == 0)) {
                 storedNum += "FizzBuzz" + "<br/>";
-            }
-
-            else if (Integer.valueOf(x) % 3 ==0){
+            } else if (Integer.valueOf(x) % 3 == 0) {
                 storedNum += "Fizz" + "<br/>";
-            }
-
-            else if (Integer.valueOf(x) % 5 ==0){
+            } else if (Integer.valueOf(x) % 5 == 0) {
                 storedNum += "Buzz" + "<br/>";
-            }
-
-            else if ((Integer.valueOf(x) % 3 !=0) && (Integer.valueOf(x) % 5 !=0)){
+            } else if ((Integer.valueOf(x) % 3 != 0) && (Integer.valueOf(x) % 5 != 0)) {
                 storedNum += x + "<br/>";
             }
 
 
-            while (x <= endValue-1) {
-                //System.out.println("<p>" + x + "</p>" );
-                x++;
-
-                if (Integer.valueOf(x) % 7 ==0){
-                    counter7 ++;
-                }
-                if (Integer.valueOf(x) % 17 ==0){
-                    counter17 ++;
-                }
-                if (Integer.valueOf(x) % 23 ==0){
-                    counter23 ++;
-                }
-
-            }
-
         }
-
-
-
-
         System.out.println();
-        return "End Value for Multiples Search: " + endValue + "<br>" + "Multiples of 7: " + String.valueOf(counter7) + "<br>" + "Multiples of 17: " + String.valueOf(counter17) + "<br>" + "Multiples of 23: " + String.valueOf(counter23) ;
-
+        return storedNum + "<br>" + "End Value for Multiples Search: " + endValue + "<br>" + "Multiples of 7: " + String.valueOf(counter7) + "<br>" + "Multiples of 17: " + String.valueOf(counter17) + "<br>" + "Multiples of 23: " + String.valueOf(counter23);
 
     }
 
 
+    //Bonus Code
+    @RequestMapping("/bonus")
+    public String Bonus ()
+
+    {
+
+        int x = 0;
+        String storedNum = "";
+        String multiples7 = "";
+        String multiples17 = "";
+        String multiples23 = "";
+        int counter7 = 0;
+        int counter17 = 0;
+        int counter23 = 0;
+        double endValue = 0;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter desired end value for the loop: ");
+        endValue = keyboard.nextDouble();
+
+        while (x <= endValue - 1) {
+            x++;
+
+
+
+            if (Integer.valueOf(x) % 7 == 0) {
+                multiples7 += x + "<br/>";
+                counter7++;
+            } else if (Integer.valueOf(x) % 17 == 0) {
+                multiples17 += x + "<br/>";
+                counter17++;
+            } else if (Integer.valueOf(x) % 23 == 0) {
+                multiples23 += x + "<br/>";
+                counter23++;
+            } else {
+                storedNum += x + "<br/>";
+            }
+
+
+        }
+
+        return storedNum + "<br>" + "End Value for Multiples Search: " + endValue + "<br>" + "Multiples of 7: " + String.valueOf(counter7) + "<br>" + "Multiples of 17: " + String.valueOf(counter17) + "<br>" + "Multiples of 23: " + String.valueOf(counter23);
+
+
+    }
 }
